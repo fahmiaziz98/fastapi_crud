4426f0775fd94d5c5826da63eb900eb9f2d8122e
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2025-04-28 09:37:10 +0700

    headers

--- 01-simple-web/service.py
+++ 01-simple-web/service.py
@@ -8,19 +8,24 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
-@app.get("/get_headers", status_code=status.HTTP_200_OK)
-async def get_header(
-    accept: str = Header(None),
-    content_type: str = Header(None),
-    user_agent: str = Header(None),
-    host: str = Header(None)
+@app.get('/get_headers', status_code=status.HTTP_200_OK)
+async def get_all_request_headers(
+    user_agent: Optional[str] = Header(None),
+    accept_encoding: Optional[str] = Header(None),
+    referer: Optional[str] = Header(None),
+    connection: Optional[str] = Header(None),
+    accept_language: Optional[str] = Header(None),
+    host: Optional[str] = Header(None),
 ):
-    request_header = {}
-    request_header["Accept"] = accept
-    request_header["Content-Type"] = content_type
-    request_header["User-Agent"] = user_agent
-    request_header["Host"] = host
-    return request_header
+    request_headers = {}
+    request_headers["User-Agent"] = user_agent
+    request_headers["Accept-Encoding"] = accept_encoding
+    request_headers["Referer"] = referer
+    request_headers["Accept-Language"] = accept_language
+    request_headers["Connection"] = connection
+    request_headers["Host"] = host
+
+    return request_headers
 
 @app.get("/items/{item_id}", status_code=status.HTTP_200_OK)
 async def read_item(item_id: int, q: Optional[str] = None) -> dict:
